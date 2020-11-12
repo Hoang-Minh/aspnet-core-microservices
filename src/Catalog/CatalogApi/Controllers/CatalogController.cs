bd6a351d099a14a5327a4c34047eac5f4aaabed0
Hoang-Minh <quataoxanh83@gmail.com>
2020-11-11 22:22:01 -0800

    Added getproductbycategory

--- src/Catalog/CatalogApi/Controllers/CatalogController.cs
+++ src/Catalog/CatalogApi/Controllers/CatalogController.cs
@@ -1,6 +1,7 @@
 ﻿using CatalogApi.Controllers.Entities;
 using CatalogApi.Repositories.Interfaces;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Net;
@@ -13,10 +14,12 @@ namespace CatalogApi.Controllers
     public class CatalogController : ControllerBase
     {
         private readonly IProductRepository _repository;
+        private readonly ILogger<CatalogController> _logger;
 
-        public CatalogController(IProductRepository repository)
+        public CatalogController(IProductRepository repository, ILogger<CatalogController> logger)
         {
             _repository = repository ?? throw new ArgumentNullException(nameof(repository));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
         }
 
         [HttpGet]
@@ -24,6 +27,31 @@ namespace CatalogApi.Controllers
         public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
         {
             var products = await _repository.GetProducts();
+            return Ok(products);
+        }
+
+        [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<Product>), (int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.NotFound)]
+        public async Task<ActionResult<Product>> GetProductById(string id)
+        {
+            var product = await _repository.GetProduct(id);
+
+            if (product == null)
+            {
+                _logger.LogError($"Product with id: {id} not found");
+                return NotFound();
+            }
+            return Ok(product);
+        }
+
+        // action will be = GetProductByCategory -- changing function name will get impacted
+        [HttpGet]
+        [Route("[action]/{category}")]
+        [ProducesResponseType(typeof(IEnumerable<Product>), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<IEnumerable<Product>>> GetProductByCategory(string categoryName)
+        {
+            var products = await _repository.GetProductByCategory(categoryName);
             return Ok(products);
         }
     }
