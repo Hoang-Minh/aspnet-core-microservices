345a9d2e27b2e4613e6966dda80d3db962948dad
Hoang-Minh <quataoxanh83@gmail.com>
2020-11-14 09:04:17 -0800

    Created Data Access Layer for BasketApi

---
+++ src/Basket/BasketApi/Data/Interfaces/IBasketContext.cs
@@ -0,0 +1,10 @@
+﻿using StackExchange.Redis;
+
+namespace BasketApi.Data.Interfaces
+{
+    // Data Access layer
+    public interface IBasketContext
+    {
+        IDatabase Redis { get; }
+    }
+}
