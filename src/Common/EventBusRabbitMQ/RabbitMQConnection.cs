15e601be9c38a3af148c74de307e2d6728bb19fb
Hoang-Minh <quataoxanh83@gmail.com>
2020-11-17 08:02:49 -0800

    Added BasketCheckoutEvent class

--- src/Common/EventBusRabbitMQ/RabbitMQConnection.cs
+++ src/Common/EventBusRabbitMQ/RabbitMQConnection.cs
@@ -1,8 +1,6 @@
 ﻿using RabbitMQ.Client;
 using RabbitMQ.Client.Exceptions;
 using System;
-using System.Collections.Generic;
-using System.Text;
 using System.Threading;
 
 namespace EventBusRabbitMQ
